5e2c0a7bcaaceec82a3ef33477cffc60bf50ca0c
awais <awaisnisar58@gmail>
2024-08-04 16:47:51 +0100

    update

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@ provider "aws" {
 resource "aws_s3_bucket" "devsecops-tf" {
   bucket = "devsecops-tf"
 }
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "devsecops-tf"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
